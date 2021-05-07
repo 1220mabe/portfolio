3942ed5b8dada7357d28f1463184def8d25dbad5
genkie <53966112+1220mabe@users.noreply.github.com>
2021-05-08 01:32:23 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,18 +9,18 @@ import VueKinesis from 'vue-kinesis'
 Vue.use(VueKinesis)
 Vue.use(VueTyperPlugin)
 Vue.use(VueParticles)
-//
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { faCoffee,faHandshake,faCode,faUserCircle,faHammer,faPen,faSchool,
-//   faSearch,faEnvelope,faMapMarkerAlt,faGlobe,faBlog,
-//   faBirthdayCake,faImage,faArchive,faToolbox,faFileCode} from '@fortawesome/free-solid-svg-icons'
-// import { faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare } from '@fortawesome/free-brands-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// library.add(faCoffee,faHandshake,faCode,faUserCircle,faHammer,faPen,
-//   faSchool,faSearch,faTwitter,faGithub,faEnvelope,faMapMarkerAlt,faGlobe,
-//   faBlog,faBirthdayCake,faHtml5,faCss3Alt,faJsSquare,faImage,faToolbox,faArchive,faFileCode)
 
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee,faHandshake,faCode,faUserCircle,faHammer,faPen,faSchool,
+  faSearch,faEnvelope,faMapMarkerAlt,faGlobe,faBlog,
+  faBirthdayCake,faImage,faArchive,faToolbox,faFileCode,faCar,faLaptop,faEdit,faCrow,faCheckCircle,faCircleNotch} from '@fortawesome/free-solid-svg-icons'
+import { faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCoffee,faHandshake,faCode,faUserCircle,faHammer,faPen,
+  faSchool,faSearch,faTwitter,faGithub,faEnvelope,faMapMarkerAlt,faGlobe,
+  faBlog,faBirthdayCake,faHtml5,faCss3Alt,faJsSquare,faImage,faToolbox,faArchive,faFileCode,faCar,faLaptop,faEdit,faCrow,faCheckCircle,faCircleNotch)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('fall-down', FallDown)
 Vue.config.productionTip = false
 
