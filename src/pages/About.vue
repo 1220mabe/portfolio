9dcb7974051f254451e3da34c329010522c2b50a
genkie <53966112+1220mabe@users.noreply.github.com>
2021-05-08 01:32:46 +0900

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -22,27 +22,27 @@
       </div>
       <div class="me">
         <h2>Education & Work Experience</h2>
-        <h3>1992. 12 <p style="display:inline;">誕生</p></h3>
+        <h3><font-awesome-icon icon="circle-notch"/> 1992. 12 <p style="display:inline;">誕生</p></h3>
         <p class="text">岩手県紫波町で中学生まで過ごす。小学校の同級生は9人という、のんのんびよりな地域で、ザリガニをとったりサッカーをして過ごす。<br><br></p>
 
-        <h3>2011. 03 <p style="display:inline;">岩手県立盛岡第一高等学校 卒業</p></h3>
+        <h3><font-awesome-icon icon="circle-notch"/> 2011. 03 <p style="display:inline;">岩手県立盛岡第一高等学校 卒業</p></h3>
         <p class="text">ラグビー部に入部し、キャプテンも勤めた。部活に気持ちがのっていたため授業は常に凌ぎに徹した。<br><br></p>
 
-        <h3>2015. 03 <p style="display:inline;">電気通信大学 情報理工学部 情報・通信工学科 卒業 </p></h3>
+        <h3><font-awesome-icon icon="circle-notch"/> 2015. 03 <p style="display:inline;">電気通信大学 情報理工学部 情報・通信工学科 卒業 </p></h3>
         <p class="text">専攻は超高層大気物理学で、卒業研究テーマはオーロラ。貧乏性すぎてMicrosoft Officeを嫌い、LaTexとApache OpenOfficeで凌ぐ。<br><br></p>
-        <h3>2017. 03 <p style="display:inline;">電気通信大学大学院情報システム学研究科 中退</p></h3>
+        <h3><font-awesome-icon icon="circle-notch"/> 2017. 03 <p style="display:inline;">電気通信大学大学院情報システム学研究科 中退</p></h3>
         <p class="text">学部生時代と研究室が変わってしまい、モチベーションを保つことができなくなり、就職へ。<br><br></p>
-        <h3>2017. 04 <p style="display:inline;">株式会社日立ソリューションズ 入社</p></h3>
+        <h3><font-awesome-icon icon="circle-notch"/> 2017. 04 <p style="display:inline;">株式会社日立ソリューションズ 入社</p></h3>
         <p class="text">愛知県で製造業向け製品管理パッケージ導入やCAD製品カスタマイズなどのSI事業を実施。<br><br></p>
-        <h3>2019. 10  <p style="display:inline;">アマノ株式会社 入社</p></h3>
+        <h3><font-awesome-icon icon="circle-notch"/> 2019. 10  <p style="display:inline;">アマノ株式会社 入社</p></h3>
         <p class="text">小・中・大企業向けに人事労務管理パッケージや入退室管理システムを導入。<br><br></p>
-        <h3>2020. 11 <p style="display:inline;">デジタルハリウッドSTUDIObyLIG上野校 入校</p></h3>
+        <h3><font-awesome-icon icon="circle-notch"/> 2020. 11 <p style="display:inline;">デジタルハリウッドSTUDIObyLIG上野校 入校</p></h3>
         <p class="text">コロナ禍により、家にいる時間が長くなり以前から興味のあったWebやデザインを学習。<br><br></p>
         <h2>Certification</h2>
-        <p class="text">●普通自動車免許</p>
-        <p class="text">●基本情報技術者</p>
-        <p class="text">●ウェブデザイン技能検定3級</p>
-        <p class="text_last">●ポケモンカードゲームイベントオーガナイザー</p>
+        <p class="text"><font-awesome-icon icon="car"/> 普通自動車免許</p>
+        <p class="text"><font-awesome-icon icon="laptop"/> 基本情報技術者</p>
+        <p class="text"><font-awesome-icon icon="edit"/> ウェブデザイン技能検定3級</p>
+        <p class="text_last"><font-awesome-icon icon="crow"/>ポケモンカードゲームイベントオーガナイザー</p>
       </div>
     </div>
   </div>
